95879367f078ff7dcc100f094e4c27f513b9c98e
DasariTejaswini13 <dasaritejaswini2006@gmail.com>
2026-06-15 16:21:32 +0530

    Add files via upload

---
+++ Week1/script.js
@@ -0,0 +1,9 @@
+// DARK MODE
+document.getElementById("darkBtn").addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+});
+
+// MOBILE MENU
+document.getElementById("menuBtn").addEventListener("click", () => {
+    document.getElementById("menu").classList.toggle("show");
+});
